9f30b2cd3ca5ac331454769646c30393842bc1ce
denis <denniiskim@gmail.com>
2024-03-17 12:03:46 +0300

    Added get by ID route

--- bird-app/app.py
+++ bird-app/app.py
@@ -9,7 +9,6 @@ from models import db, Bird
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,9 +16,23 @@ db.init_app(app)
 api = Api(app)
 
 class Birds(Resource):
-
     def get(self):
-        birds = [bird.to_dict() for bird in Bird.query.all()]
-        return make_response(jsonify(birds), 200)
+        try:
+            birds = [bird.to_dict() for bird in Bird.query.all()]
+            return make_response(jsonify({'birds': birds}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 500)
+
+class BirdByID(Resource):
+    def get(self, id):
+        bird = Bird.query.filter_by(id=id).first()
+        if bird:
+            return make_response(jsonify(bird.to_dict()), 200)
+        else:
+            return make_response(jsonify({'error': 'Bird not found'}), 404)
 
 api.add_resource(Birds, '/birds')
+api.add_resource(BirdByID, '/birds/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
